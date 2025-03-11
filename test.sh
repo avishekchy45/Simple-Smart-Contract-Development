bb734a0ee510a485989db550918ba78778641a89
avishekchy45 <avishekchy45@gmail.com>
2025-03-12 00:34:58 +0600

    Commands Added 👨🏼‍🏫

---
+++ test.sh
@@ -0,0 +1,16 @@
+npm install -g ganache-cli
+
+npm install -g truffle
+
+mkdir SimpleSmartContract
+cd SimpleSmartContract
+
+truffle init
+
+truffle compile
+
+truffle migrate
+
+truffle console
+
+truffle test
